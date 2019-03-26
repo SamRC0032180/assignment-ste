686909d695fd2c170ebb32a8ae84334840b541e0
RC0032180 <RC0032180@CRO-24-04>
2019-03-26 10:44:06 +0000

    Add project files.

---
+++ assignment ste/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_ste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            double resistor1 = double.Parse(textBox1.Text);
+            double resistor2 = double.Parse(textBox2.Text);
+            double resistor3 = double.Parse(textBox3.Text);
+            double resistor4 = double.Parse(textBox4.Text);
+
+            totalresistance = resistor1 + resistor2 + resistor3 + resistor4;
+            MessageBox.Show("total resistance =" + totalresistance.ToString("0.0") + "ohms");
+
+
+        }
+    }
+}
